a7e5597a417788fbc065bfe6d544a1a6144aa216
sinshu <sinshu@DESKTOP-SVO3ACV>
2021-10-25 20:50:49 +0900

    초기 커밋.

---
+++ study1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; // 헤더파일 포함한 부분 ex) #include 같은.. , prologue/prolog 라고 함
+
+namespace study1 // 코딩을 하는 몸체부분
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
